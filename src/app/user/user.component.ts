329d35ee75d9babe174ee53ee6ce62bcb4fef272
Saban Uenlue - netTrek <us@netTrek.de>
2019-12-03 13:45:05 +0100

    ngClass

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -14,40 +14,9 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 } )
 export class UserComponent implements OnInit {
 
-  name = 'saban';
-  filename = 'cat1.jpeg';
-  btnRole = 'button';
-  fontColor = 'red';
-  catSize = 50;
-  html = `hello <strong>world</strong>! <script>alert('hab dich .....')</script>`;
-  currentClass = 'red';
-  selected: string;
-
   constructor() {
   }
 
   ngOnInit() {
-  }
-
-  chgName() {
-    // debugger
-    this.name = 'peter müller';
-    this.filename = 'cat2.jpeg';
-    this.fontColor = 'blue';
-  }
-/*
-  // wenn möglich ohne Methoden
-  getName(): string {
-    console.log ( 'getName' );
-    return this.name;
-  }*/
-  toggleClass() {
-    this.currentClass =
-      this.currentClass === 'red' ?
-      'blue' : 'red';
-  }
-
-  select( name: string ) {
-    this.selected = name;
   }
 }
