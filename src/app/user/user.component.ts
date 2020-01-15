73cc495ea63954fcaa4111092d5b84b731b85721
Saban Uenlue - netTrek <us@netTrek.de>
2020-01-15 15:13:08 +0100

    encapsulation, :host ::ng-deep

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,14 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
-@Component({
-  selector: 'gfk-user',
+@Component ( {
+  selector   : 'gfk-user', // Knoten-Name innerhalb der HTML
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.scss']
-})
+  /*  template: `
+      <h1>
+        hello {{name}}
+      </h1>
+      <h2>world</h2>
+    `,*/
+  styleUrls  : [ './user.component.scss' ],
+  // encapsulation: ViewEncapsulation.Emulated // attribute -def.
+  // encapsulation: ViewEncapsulation.None // ohne attrib. nutzung
+  // encapsulation: ViewEncapsulation.ShadowDom
+  /*  styles: [ `
+              h1 {
+                color: red;
+              }
+              `]*/
+} )
 export class UserComponent implements OnInit {
   name = 'Saban';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
