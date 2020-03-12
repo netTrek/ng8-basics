b88ca262d908e2124d5b53ed4bdc63243100b5dc
Saban Uenlue - netTrek <us@netTrek.de>
2020-03-12 13:05:43 +0100

    ng-content

--- src/app/user/user-list/user-list.component.ts
+++ src/app/user/user-list/user-list.component.ts
@@ -1,13 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
 
-@Component({
-  selector: 'cis-user-list',
+@Component ( {
+  selector   : 'cis-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.scss']
-})
+  styleUrls  : ['./user-list.component.scss']
+} )
 export class UserListComponent implements OnInit {
 
-  constructor() { }
+  userList: User[] = [
+    {
+      firstname: 'saban',
+      lastname : 'ünlü'
+    },
+    {
+      firstname: 'peter',
+      lastname : 'müller'
+    }
+  ];
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
