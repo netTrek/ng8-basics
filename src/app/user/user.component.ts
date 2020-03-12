d1f0a58163673748c834019a225c07b35de66447
Saban Uenlue - netTrek <us@netTrek.de>
2020-03-12 09:53:51 +0100

    style samples

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,9 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'cis-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.scss']
+/*  template: `
+  <ul>
+  <li>1</li>
+  <li>2</li>
+  <li>3</li>
+  </ul>
+  `,*/
+  styleUrls: ['./user.styles.scss',
+              './user.component.scss'],
+  encapsulation: ViewEncapsulation.Emulated
+  // encapsulation: ViewEncapsulation.ShadowDom
+  /*styles: [`:host {
+    display: block;
+    background-color: red;
+  }`]*/
 })
 export class UserComponent implements OnInit {
 
