063f9b66a85892030212085cd4a55cb890c4b596
Saban Uenlue - netTrek <us@netTrek.de>
2020-03-13 15:26:46 +0100

    - router get params val

--- src/app/user/user-detail/user-detail.component.ts
+++ src/app/user/user-detail/user-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs/operators';
 
 @Component ( {
   selector   : 'cis-user-detail',
@@ -6,11 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls  : ['./user-detail.component.scss']
 } )
 export class UserDetailComponent implements OnInit {
+  userId: number;
 
-  constructor() {
+  constructor( private $route: ActivatedRoute ) {
   }
 
   ngOnInit(): void {
+    this.$route.paramMap
+        .pipe (
+          map ( pMap => + pMap.get ( 'id' ) )
+        )
+        .subscribe ( n => this.userId = n );
   }
 
 }
