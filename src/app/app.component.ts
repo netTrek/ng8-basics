7733e2d7da71f0fd3e0f69cdaacaa7a7d46b5d8f
Saban Uenlue - netTrek <us@netTrek.de>
2020-03-13 13:39:36 +0100

    http sample completed 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AppErrorService } from './app-error.service';
+import { filter, skipWhile } from 'rxjs/operators';
 
 @Component ( {
   selector   : 'cis-root',
@@ -10,6 +11,8 @@ export class AppComponent {
   title = 'softconcis';
 
   constructor( public $error: AppErrorService ) {
+    this.$error.error$.pipe ( filter ( n => !!n ) )
+        .subscribe ( n => console.log ( 'ups ...', n ) );
   }
 
   changeName() {
